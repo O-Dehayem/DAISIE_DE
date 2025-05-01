d67df32b6a6ca62e54c79235ba1793b28498925b
Ornela Dehayem Nanwou <o.dehayem@gmail.com>
2025-05-01 09:32:06 +0200

    Update DAISIE_DE_logpES_max_age_coltime.R

--- DAISIE_DE_logpES_max_age_coltime.R
+++ DAISIE_DE_logpES_max_age_coltime.R
@@ -1,131 +1,139 @@
-#rm(list=ls())
-library(deSolve)
-library(DAISIE)
-library(pracma)
-###############################################################################
-### fonction to calculate the likelihood of observing an endemic singleton lineage
-### with the max age colonization time t1
-###############################################################################
+#' @name DAISIE_DE_logpES_max_age_coltime
+#' @title Function to calculate the likelihood of observing an endemic singleton lineage on the island
+#' with maximum age of colonization
+#' @description This function calculates the log-likelihood of observing an endemic singleton lineage on an island
+#' for which the exact colonization time is unknown, but the maximum of colonization is known.
+#'
+#' @inheritParams default_params_doc_DAISIE_DE
+#' @return The output is a numeric value representing the log-likelihood of observing an endemic singleton lineage
+#' for which the maximum age of colonization is given.
+#' \item{logL1b}{ The log-likelihood value computed based on a system of differential equations.}
+#'
+#' @examples
+#'
+#' # Select a dataset from a DAISIE package
+#'
+#' data(Macaronesia_datalist)
+#'
+#'Azores <- Macaronesia_datalist$Azores
+#' # Select an endemic lineage in the dataset
+#' i <- 16
+#' # Define example parameters
+#' pars1 <- c(0.2, 0.1, 0.05, 0.02, 0.03)
+#'
+#' # choose the method to solve the system of differential equations
+#' log_likelihood <- DAISIE_DE_logpES_max_age_coltime(datalist, i, pars1, methode = "lsodes", reltolint = 1e-16, abstolint = 1e-16)
+#'
+#' print(log_likelihood)
 
-### Using D-E approach
 
-
-# pars1[1] corresponds to the Cladogenesis rate
-# pars1[2] corresponds to the Extinction rate of endemic lineages
-# pars1[3] corresponds to the Extinction rate of non-endemic lineages
-# pars1[4] = corresponds to the Colonization rate
-# pars1[5] = corresponds to the Anagenesis rate
-# if equal_extinction = TRUE, the extinction rates of endemic and non-endemic species are equal.
-# else, the are estimated separately
-
-### Using D-E approach
-DAISIE_DE_logpES_max_age_coltime <- function(datalist, 
+#' @export DAISIE_DE_logpES_max_age_coltime
+DAISIE_DE_logpES_max_age_coltime <- function(datalist,
                                              i,
                                              pars1,
                                              methode,
-                                             rtol, 
-                                             atol,
-                                             equal_extinction = FALSE) {
-  
-  if (equal_extinction) {
-    pars1[3] <- pars1[2]
-  }
-  
-  t0 <- datalist[[1]]$island_age
-  t1 <- datalist[[i]]$branching_times[2]
+                                             reltolint,
+                                             abstolint) {
+
+  brts = datalist[[i]]$branching_times
+  missnumspec = datalist[[i]]$missing_species
+
+  t0 <- brts[1]
+  t1 <- brts[2]
   tp <- 0
   parameters <- pars1
-  
+
   # Define system of equations for interval [t1, tp]
   interval1 <- function(t, state, parameters) {
     with(as.list(c(state, parameters)), {
-      dD1 <- -(pars1[1] + pars1[2]) * D1 + 2 * pars1[1] * D1 * E1
-      
-      
-      dD02 <- -pars1[4] * D02 + pars1[4] * Dm2
-      
-      dD03 <- -pars1[4] * D03 + pars1[4] * Dm3
-      
-      
+      dDE <- -(pars1[1] + pars1[2]) * DE + 2 * pars1[1] * DE * E
+
+      dDA2 <- -pars1[4] * DA2 + pars1[4] * Dm2
+
+      dDA3 <- -pars1[4] * DA3 + pars1[4] * Dm3
+
       dDm1 <- -(pars1[5] + pars1[1] + pars1[3] + pars1[4]) * Dm1 +
-        (pars1[3] + pars1[5] * E1 + pars1[1] * E1^2)* D02 + pars1[4] * (Dm2)
-      
-      
-      
+        (pars1[3] + pars1[5] * E + pars1[1] * E^2)* DA2 + pars1[4] * (Dm2)
+
       dDm2 <- -(pars1[5] + pars1[1] + pars1[3]) * Dm2 +
-        (pars1[3] + pars1[5] * E1 + pars1[1] * E1^2)* D02 +
-        (pars1[5] * D1 + 2 * pars1[1] * D1 * E1 ) * D03 
-      
-      
-      
-      dDm3 <- -(pars1[5] + pars1[1] + pars1[2]) * Dm3 +
-        (pars1[3] + pars1[5] * E1 + pars1[1] * E1^2) * D03 
-      
-      
-      dE1 <- pars1[2] - (pars1[1] + pars1[2]) * E1 + pars1[1] * E1^2
-      
-      list(c(dD1, dD02, dD03, dDm1, dDm2, dDm3, dE1))
+        (pars1[3] + pars1[5] * E + pars1[1] * E^2)* DA2 +
+        (pars1[5] * DE + 2 * pars1[1] * DE * E ) * DA3
+
+      dDm3 <- -(pars1[5] + pars1[1] + pars1[3]) * Dm3 +
+        (pars1[3] + pars1[5] * E + pars1[1] * E^2) * DA3
+
+      dE <- pars1[2] - (pars1[1] + pars1[2]) * E + pars1[1] * E^2
+
+      list(c(dDE, dDA2, dDA3, dDm1, dDm2, dDm3, dE))
     })
   }
-  
-  
+
+
   # Initial conditions
-  initial_conditions1 <- c(D1 = 1, D02 = 0, D03 = 1, Dm1 = 0, Dm2 = 0, Dm3 = 0, E1 = 0)
-  
-  
+  number_of_species <- length(brts) -1
+  number_of_missing_species <- missnumspec
+  ro <- number_of_species / (number_of_missing_species + number_of_species)
+
+  if (datalist[[i]]$missing_species == 0)
+
+  {
+    initial_conditions1 <- c(DE = 1, DA2 = 0, DA3 = 1, Dm1 = 0, Dm2 = 0, Dm3 = 0, E = 0)
+  }
+  else
+
+  {
+    initial_conditions1 <- c(DE = ro, DA2 = 0, DA3 = 1, Dm1 = 0, Dm2 = 0, Dm3 = 0, E = 1 - ro)
+  }
+
+
+
   # Define system of equations for interval [t0, t1]
   interval2 <- function(t, state, parameters) {
     with(as.list(c(state, parameters)), {
-      
-      dD0 <- -pars1[4] * D0 + pars1[4] * Dm1
-      
-      dDm1 <- -(pars1[5] + pars1[1] + pars1[3]) * Dm1 + (pars1[5] * E1 + pars1[1] * E1^2 + pars1[3]) * D0
-      
-      dE1 <- pars1[2] - (pars1[1] + pars1[2]) * E1 + pars1[1] * E1^2
-      
-      list(c(dD0, dDm1, dE1))
+
+      dDA1 <- -pars1[4] * DA1 + pars1[4] * Dm1
+
+      dDm1 <- -(pars1[5] + pars1[1] + pars1[3]) * Dm1 + (pars1[5] * E + pars1[1] * E^2 + pars1[3]) * DA1
+
+      dE <- pars1[2] - (pars1[1] + pars1[2]) * E + pars1[1] * E^2
+
+      list(c(dDA1, dDm1, dE))
     })
   }
-  
-  
-  
+
   # Time sequence for interval [t1, tp]
   time1 <- c(tp, t1)
-  
+
   # Solve the system for interval [t1, tp]
-  solution1 <- ode(y = initial_conditions1,
-                   times = time1,
-                   func = interval1,
-                   parms = parameters,
-                   method = methode,
-                   rtol = rtol, 
-                   atol = atol)
-  
+  solution1 <- deSolve::ode(y = initial_conditions1,
+                            times = time1,
+                            func = interval1,
+                            parms = parameters,
+                            method = methode,
+                            rtol = reltolint,
+                            atol = abstolint)
+
   # Initial conditions
-  initial_conditions2 <- c(D0 = solution1[, "D02"][[2]],
+  initial_conditions2 <- c(DA1 = solution1[, "DA2"][[2]],
                            Dm1 = solution1[, "Dm1"][[2]],
-                           E1 = solution1[, "E1"][[2]])
-  
+                           E = solution1[, "E"][[2]])
+
   # Time sequence for interval [t0, t1]
   time2 <- c(t1, t0)
-  
+
   # Solve the system for interval [t0, t1]
-  solution2 <- ode(y = initial_conditions2,
-                   times = time2,
-                   func = interval2,
-                   parms = parameters, 
-                   method = methode,
-                   rtol = rtol, 
-                   atol = atol)
-  
+  solution2 <- deSolve::ode(y = initial_conditions2,
+                            times = time2,
+                            func = interval2,
+                            parms = parameters,
+                            method = methode,
+                            rtol = reltolint,
+                            atol = abstolint)
+
   # Extract log-likelihood
-  L1 <- solution2[, "D0"][[2]]
+  L1 <- solution2[, "DA1"][[2]]
   logL1b <- log(L1)
-  
+
   return(logL1b)
-  
+
 }
-
-
-
-
